76de40d8bc8cead7416209dff605a95820b4e659
Robert Kleniewski <robert.kleniewski@gmail.com>
2017-08-26 12:04:18 +0200

    sync

--- commonfun.sh
+++ commonfun.sh
@@ -2,8 +2,10 @@
 
 function _rmc() {
         # kill all containers and remove them along with unused docker networks
-        if [[ .`docker ps -qa | wc -l`. != .0. ]]; then
+        if [[ .`docker ps -q | wc -l`. != .0. ]]; then
                 docker kill $(docker ps -qa)
+        fi
+        if [[ .`docker ps -qa | wc -l`. != .0. ]]; then
                 docker rm $(docker ps -qa)
         fi
 	# rm all images
@@ -13,4 +15,8 @@ function _rmc() {
 
 function _rmi() {
 	docker images | grep "^hyperledger" | awk '{print $3}' | while read AA; do docker rmi -f $AA; done
+}
+
+function _rmidev() {
+	docker images | grep "^dev" | awk '{print $3}' | while read AA; do docker rmi -f $AA; done
 }
